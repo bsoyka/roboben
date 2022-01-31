a9130449e9593a6ab06610552a1cadbcb8dc471f
Ben Soyka <bensoyka@icloud.com>
2022-01-30 20:52:49 -0700

    Create ping command

---
+++ bot/exts/info/ping.py
@@ -0,0 +1,35 @@
+"""Ping command."""
+
+from discord import Color, Embed
+from discord.ext.commands import Cog, Context, command
+
+from bot.bot import RobobenBot
+
+ROUND_LATENCY = 2
+
+
+class Latency(Cog):
+    """The bot's ping command."""
+
+    category = "Information"
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @command()
+    async def ping(self, ctx: Context) -> None:
+        """Shows the Discord API latency"""
+        embed = Embed(title="Pong!", color=Color.green())
+
+        embed.add_field(
+            name="Discord API latency",
+            value=f"{self.bot.latency * 1000:.{ROUND_LATENCY}f} ms",
+            inline=False,
+        )
+
+        await ctx.send(embed=embed)
+
+
+def setup(bot: RobobenBot):
+    """Loads the latency cog."""
+    bot.add_cog(Latency(bot))
